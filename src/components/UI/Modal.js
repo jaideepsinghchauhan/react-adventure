d34e4c46695500895161c50ee8b8e75f769147e9
Singh, Jaideep <jaideep.singh@dell.com>
2022-01-25 17:18:38 +0530

    adding toggle logic for overlay and modal

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -3,7 +3,7 @@ import ReactDom from "react-dom";
 import classes from "./Modal.module.css";
 
 const Backdrop = (props) => {
-  return <div className={classes.backdrop}></div>;
+  return <div className={classes.backdrop} onClick={props.onClose}></div>;
 };
 const ModalOverlay = (props) => {
   return (
@@ -16,7 +16,7 @@ const ModalOverlay = (props) => {
 const Modal = (props) => {
     const portalElement = document.getElementById('overlays')
     return <React.Fragment >
-                {ReactDom.createPortal(<Backdrop/>, portalElement )}
+                {ReactDom.createPortal(<Backdrop onClose={props.onClose}/>, portalElement )}
                 {ReactDom.createPortal(<ModalOverlay> {props.children}</ModalOverlay>, portalElement )}
     </React.Fragment>
 };
